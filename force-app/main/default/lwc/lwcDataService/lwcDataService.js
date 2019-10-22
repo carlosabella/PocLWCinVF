40e7c6a6acb53b0991c8e1b18be2e53ae365e6cf
Carlos Garcia <cgarcia@rootadmins-MacBook-Air-5.local>
2019-10-22 13:07:55 +0200

    added tab

--- force-app/main/default/lwc/lwcDataService/lwcDataService.js
+++ force-app/main/default/lwc/lwcDataService/lwcDataService.js
@@ -4,5 +4,5 @@ import { LightningElement, api } from 'lwc';
 export default class LwcDataService extends LightningElement {
     // The record page provides recordId and objectApiName
     @api recordId = '0012o00002NQS5T';
-    @api objectApiName = 'Account';
+    @api objectApiName = 'Account'; 
 }
