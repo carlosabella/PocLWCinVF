4c30cf0504cad3e724695120b218d621b96a2a3c
Carlos Garcia <cgarcia@rootadmins-MacBook-Air-5.local>
2019-10-22 13:05:06 +0200

    add dataservice layer

---
+++ force-app/main/default/lwc/lwcDataService/lwcDataService.js
@@ -0,0 +1,8 @@
+import { LightningElement, api } from 'lwc';
+
+
+export default class LwcDataService extends LightningElement {
+    // The record page provides recordId and objectApiName
+    @api recordId = '0012o00002NQS5T';
+    @api objectApiName = 'Account';
+}
