72db7f3afa01d1c23812dd7562db10545e70abd1
Carlos Garcia <cgarcia@rootadmins-MacBook-Air-5.local>
2019-10-25 17:19:31 +0200

    a

--- force-app/main/default/lwc/lwcDataService/lwcDataService.js
+++ force-app/main/default/lwc/lwcDataService/lwcDataService.js
@@ -3,6 +3,12 @@ import { LightningElement, api } from 'lwc';
 
 export default class LwcDataService extends LightningElement {
     // The record page provides recordId and objectApiName
+    
     @api recordId = '0012o00002NQS5T';
     @api objectApiName = 'Account'; 
+    myfunction()    {
+        // eslint-disable-next-line no-debugger
+        debugger;
+    }
+
 }
